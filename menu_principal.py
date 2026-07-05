d5876723ffafba3c83cd8349dbd498a3dcbabc38
Helder <hrasantos.cic@uesc.br>
2026-07-05 13:04:39 -0300

    Comentarios adicionados

--- menu_principal.py
+++ menu_principal.py
@@ -1,10 +1,26 @@
 import tkinter as tk
 
+# Separando as telas em arquivos diferentes, isso possibilita uma melhor 
+# organização do código e facilita uma possível manutenção posterior 
+
 from telas.tela_vendas import TelaVendas
 from telas.tela_estoque import TelaEstoque
 from telas.tela_relatorios import TelaRelatorios
 
+# Aqui encontramos a classe principal do sistema de interface
+# Como é possível notar, essa é uma classe filha da classe Tk
+# Deixar a classe como filha permite usar os métodos e atributos da classe Tk como se fossem da própria classe
+
 class Sistema(tk.Tk):
+    """Classe principal do sistema de interface
+    Atributos: Nenhum
+    Métodos:
+        __init__: Cria a janela principal do sistema
+        interface: Chamada automaticamente pelo método __init__, cria a interface do sistema
+        abrir_vendas: Abre a aba de vendas do sistema
+        abrir_estoque: Abre a aba de gerenciamento de produtos e estoque
+        abrir_relatorios: Abre a aba de relatórios financeiros do sistema
+    """
     def __init__(self):
         super().__init__()
         self.title('Menu Principal')
