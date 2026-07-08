448a0d8132110573715a44cfca2ff1291de51f1b
ElderHelder <hrasantos.cic@uesc.br>
2026-07-08 16:14:01 -0300

    Add docstring to PersistenciaDados class
    Added docstring to PersistenciaDados class explaining its purpose and methods.

--- dados/persistencia.py
+++ dados/persistencia.py
@@ -1,7 +1,16 @@
+#Biblioteca do python para serialização e deserialização
 import pickle
+#Módulo nativo de python que permite interagir com o sistema operacional
 import os
 
 class PersistenciaDados:
+    """
+    Classe para persistência de dados(salvar dados)
+    Métodos:
+    __init__: Inicializa, recebe o nome do arquivo do banco de dados, automaticamente definido como dados_sistema.dat
+    salvar: Método que permite salvar qualquer informação, recebe o dado que será salvo
+    carregar: Método que permite carregar dados previamente salvos
+    """
     def __init__(self, arquivo_banco="dados_sistema.dat"):
         self.arquivo_banco = arquivo_banco
 
