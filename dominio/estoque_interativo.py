956f763511e29ae661da43631622e189bc8c53de
pgfreitas <phgfreitas.cic@uesc.br>
2026-07-08 14:15:49 -0300

    Update estoque_interativo.py

--- dominio/estoque_interativo.py
+++ dominio/estoque_interativo.py
@@ -1,12 +1,14 @@
 from dados.persistencia import PersistenciaDados
 
 def add_estoque(janela_formulario):
-    produto_nome = janela_formulario.entry_produto.get()
-    preco_produto = janela_formulario.entry_preco.get()
+    produto_nome = janela_formulario.entry_produto.get().strip()
+    preco_produto = janela_formulario.entry_preco.get().strip()
+    qtd_produto = janela_formulario.entry_qtd.get().strip()
 
-    if produto_nome and preco_produto:
+    if produto_nome and preco_produto and qtd_produto:
         try:
             preco_float = float(preco_produto.replace(',', '.'))
+            qtd_int = int(qtd_produto)
             
             banco = PersistenciaDados()
             dados = banco.carregar()
@@ -14,12 +16,16 @@ def add_estoque(janela_formulario):
             if isinstance(dados.get("estoque"), list):
                 dados["estoque"] = {}
                 
-            dados["estoque"][produto_nome] = preco_float
+            dados["estoque"][produto_nome] = {
+                "preco": preco_float,
+                "quantidade": qtd_int
+            }
+            
             banco.salvar(dados)
 
-            janela_formulario.master.adicionar_item_na_lista(produto_nome, f"{preco_float:.2f}")
+            janela_formulario.master.adicionar_item_na_lista(produto_nome, f"{preco_float:.2f}", str(qtd_int))
             janela_formulario.destroy()
         except ValueError:
-            janela_formulario.master.lbl_info.config(text="Erro: O preço deve ser um número!", fg="red")
+            janela_formulario.master.lbl_info.config(text="Erro: Preço ou quantidade inválidos!", fg="red")
     else:
         janela_formulario.master.lbl_info.config(text="Erro: Preencha todos os campos!", fg="red")
